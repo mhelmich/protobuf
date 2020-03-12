1a11e8b2df795524cc4e91eb76f2337a854ca49e
Marco <marco.helmich@live.com>
2020-03-12 15:04:37 -0700

    build a avro schema generator for protobuf

--- plugin/avroschema/avroschema.go
+++ plugin/avroschema/avroschema.go
@@ -1,17 +1,74 @@
 package avroschema
 
 import (
+	"errors"
 	"fmt"
-	"os"
+	"strconv"
+	"strings"
 
 	"github.com/gogo/protobuf/gogoproto"
+	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
 	"github.com/gogo/protobuf/protoc-gen-gogo/generator"
 )
+
+func init() {
+	generator.RegisterPlugin(newAvroSchema())
+}
+
+var errTypeNotFound = errors.New("type not found")
+
+type avroField interface {
+	schema() string
+}
+
+type simpleField struct {
+	name string
+	typ  string
+}
+
+func (f *simpleField) schema() string {
+	return fmt.Sprintf("{\"name\": \"%s\", \"type\": \"%s\"}", f.name, f.typ)
+}
+
+type complexField struct {
+	name      string
+	namespace string
+	typ       string
+	fields    []avroField
+}
+
+func (f *complexField) schema() string {
+	var fields []string
+	for _, field := range f.fields {
+		fields = append(fields, field.schema())
+	}
+
+	return fmt.Sprintf("{\"name\": \"%s\", \"type\": \"record\", \"namespace\": \"%s\", \"fields\": [ %s ]}", f.name, f.namespace, strings.Join(fields, ", "))
+}
+
+type arrayField struct {
+	name string
+	typ  string
+}
+
+func (f *arrayField) schema() string {
+	return fmt.Sprintf("{\"name\": \"%s\", \"type\": {\"type\": \"array\", \"items\": \"%s\"}}", f.name, f.typ)
+}
+
+type enumField struct {
+	name    string
+	symbols []string
+}
+
+func (f *enumField) schema() string {
+	return fmt.Sprintf("{\"name\": \"%s\", \"type\": {\"type\": \"enum\", \"symbols\": [ %s ]}}", f.name, strings.Join(f.symbols, ", "))
+}
 
 type avroschema struct {
 	*generator.Generator
 	generator.PluginImports
-	localName string
+	fileName string
+	seen     map[string]*complexField
 }
 
 func newAvroSchema() *avroschema {
@@ -24,33 +81,161 @@ func (p *avroschema) Name() string {
 
 func (p *avroschema) Init(g *generator.Generator) {
 	p.Generator = g
+	p.seen = make(map[string]*complexField)
 }
 
 func (p *avroschema) Generate(file *generator.FileDescriptor) {
 	p.PluginImports = generator.NewPluginImports(p.Generator)
-	p.localName = generator.FileName(file)
-	fmt.Fprintf(os.Stderr, "ERROR: %s ======================================================\n", p.localName)
+	p.fileName = generator.FileName(file)
 	for _, message := range file.Messages() {
 		if gogoproto.HasAvroSchema(file.FileDescriptorProto, message.DescriptorProto) {
-			p.processMessage(message)
+			p.createMessageStub(message)
 		}
 	}
 }
 
-func (p *avroschema) processMessage(message *generator.Descriptor) {
-	ccTypeName := generator.CamelCaseSlice(message.TypeName())
-	p.P(`func (this *`, ccTypeName, `) AvroSchema() string {`)
+func (p *avroschema) createMessageStub(message *generator.Descriptor) {
+	ccTypeName := message.GetName()
+	schema := p.processMessage(message)
+	p.P(`func AvroSchemaFor`, ccTypeName, `() string {`)
 	p.In()
-	p.P(`if this == nil {`)
-	p.In()
-	p.P(`return "nil"`)
-	p.Out()
-	p.P(`}`)
-	p.P(`return ""`)
+	p.P(fmt.Sprintf("return %s", strconv.Quote(schema.schema())))
 	p.Out()
 	p.P(`}`)
 }
 
-func init() {
-	generator.RegisterPlugin(newAvroSchema())
+func (p *avroschema) processMessage(message *generator.Descriptor) *complexField {
+	cf, ok := p.seen[generator.CamelCaseSlice(message.TypeName())]
+
+	if !ok {
+		cf = &complexField{
+			typ:       "record",
+			namespace: p.fileName,
+			name:      generator.CamelCaseSlice(message.TypeName()),
+			fields:    make([]avroField, 0),
+		}
+		p.seen[generator.CamelCaseSlice(message.TypeName())] = cf
+	}
+
+	for _, field := range message.Field {
+		fs, err := p.processField(message, field)
+		if err != nil {
+			panic(fmt.Sprintf("Rats! %s", err.Error()))
+		}
+		cf.fields = append(cf.fields, fs)
+	}
+
+	return cf
+}
+
+func (p *avroschema) processField(message *generator.Descriptor, field *descriptor.FieldDescriptorProto) (avroField, error) {
+	if field.IsRepeated() {
+		return p.getArrayField(message, field)
+	}
+
+	switch *field.Type {
+	case descriptor.FieldDescriptorProto_TYPE_DOUBLE,
+		descriptor.FieldDescriptorProto_TYPE_FLOAT,
+		descriptor.FieldDescriptorProto_TYPE_INT64,
+		descriptor.FieldDescriptorProto_TYPE_UINT64,
+		descriptor.FieldDescriptorProto_TYPE_INT32,
+		descriptor.FieldDescriptorProto_TYPE_UINT32,
+		descriptor.FieldDescriptorProto_TYPE_FIXED64,
+		descriptor.FieldDescriptorProto_TYPE_FIXED32,
+		descriptor.FieldDescriptorProto_TYPE_BOOL,
+		descriptor.FieldDescriptorProto_TYPE_STRING,
+		descriptor.FieldDescriptorProto_TYPE_SFIXED32,
+		descriptor.FieldDescriptorProto_TYPE_SFIXED64,
+		descriptor.FieldDescriptorProto_TYPE_SINT32,
+		descriptor.FieldDescriptorProto_TYPE_SINT64,
+		descriptor.FieldDescriptorProto_TYPE_BYTES:
+		return p.getSimpleAvroField(message, field)
+
+	case descriptor.FieldDescriptorProto_TYPE_MESSAGE:
+		return p.getComplexField(message, field)
+	case descriptor.FieldDescriptorProto_TYPE_ENUM:
+		return p.getEnumField(message, field)
+
+	case descriptor.FieldDescriptorProto_TYPE_GROUP:
+		// do nothing - groups are deprecated
+		return nil, nil
+
+	default:
+		p.Fail("unknown type for ", field.GetName())
+		return nil, errTypeNotFound
+	}
+}
+
+func (p *avroschema) getSimpleAvroField(message *generator.Descriptor, field *descriptor.FieldDescriptorProto) (*simpleField, error) {
+	return &simpleField{
+		name: p.GetFieldName(message, field),
+		typ:  p.getAvroTypeForProtobufType(field.Type),
+	}, nil
+}
+
+func (p *avroschema) getArrayField(message *generator.Descriptor, field *descriptor.FieldDescriptorProto) (*arrayField, error) {
+	return &arrayField{
+		name: p.GetFieldName(message, field),
+		typ:  p.getAvroTypeForProtobufType(field.Type),
+	}, nil
+}
+
+func (p *avroschema) getComplexField(message *generator.Descriptor, field *descriptor.FieldDescriptorProto) (*complexField, error) {
+	cached, ok := p.seen[generator.CamelCase(field.GetName())]
+	if ok {
+		return cached, nil
+	}
+
+	f := &complexField{
+		name:      generator.CamelCaseSlice(message.TypeName()),
+		typ:       "record",
+		namespace: p.fileName,
+		fields:    make([]avroField, 0),
+	}
+
+	p.seen[generator.CamelCase(field.GetName())] = f
+	return f, nil
+}
+
+func (p *avroschema) getEnumField(message *generator.Descriptor, field *descriptor.FieldDescriptorProto) (*enumField, error) {
+	return &enumField{
+		name: field.GetName(),
+	}, nil
+}
+
+func (p *avroschema) getAvroTypeForProtobufType(t *descriptor.FieldDescriptorProto_Type) string {
+	switch *t {
+	case descriptor.FieldDescriptorProto_TYPE_DOUBLE:
+		return "double"
+	case descriptor.FieldDescriptorProto_TYPE_FLOAT:
+		return "float"
+	case descriptor.FieldDescriptorProto_TYPE_INT64:
+		return "long"
+	case descriptor.FieldDescriptorProto_TYPE_UINT64:
+		return "long"
+	case descriptor.FieldDescriptorProto_TYPE_INT32:
+		return "int"
+	case descriptor.FieldDescriptorProto_TYPE_UINT32:
+		return "int"
+	case descriptor.FieldDescriptorProto_TYPE_FIXED64:
+		return "long"
+	case descriptor.FieldDescriptorProto_TYPE_FIXED32:
+		return "int"
+	case descriptor.FieldDescriptorProto_TYPE_BOOL:
+		return "boolean"
+	case descriptor.FieldDescriptorProto_TYPE_STRING:
+		return "string"
+	case descriptor.FieldDescriptorProto_TYPE_SFIXED32:
+		return "int"
+	case descriptor.FieldDescriptorProto_TYPE_SFIXED64:
+		return "long"
+	case descriptor.FieldDescriptorProto_TYPE_SINT32:
+		return "int"
+	case descriptor.FieldDescriptorProto_TYPE_SINT64:
+		return "long"
+	case descriptor.FieldDescriptorProto_TYPE_BYTES:
+		return "bytes"
+	default:
+		return "nil"
+	}
 }
